d31698357f7d27b194d08d17450c35988782b735
Jesse Sheehan <jps111@uclive.ac.nz>
2018-10-08 14:47:38 +1300

    Began work on processLineClears (UNTESTED)

--- physics.c
+++ physics.c
@@ -18,3 +18,32 @@ void rotateActivePiece(Game* game, uint8_t direction)
 {
 
 }
+
+/**
+ * Checks each row in the game board for any line clears.
+ * If any are found, they are removed and everything above is moved down.
+ * Returns the number of lines cleared.
+ */
+uint8_t processLineClears(Game* game)
+{
+    uint8_t num_clears = 0;
+    
+    uint8_t i = 0;
+    for (; i < GAME_BOARD_HEIGHT; i++) {
+
+        uint8_t j = 0;
+        for (; j < GAME_BOARD_WIDTH; j++) {
+
+            if (game->board[i][j] != 1) {
+                break;
+            }
+
+        }
+
+        if (j == GAME_BOARD_WIDTH) {
+            num_clears++;
+        }
+
+    }
+    return num_clears;
+}
--- physics.h
+++ physics.h
@@ -1,6 +1,7 @@
 #ifndef PHYSICS_H_
 #define PHYSICS_H_
 
+#include <stdint.h>
 #include "tetris.h"
 
 /**
@@ -15,5 +16,12 @@ void applyGravity(Game* game);
  * A rotation will sometimes cause a kick to occur.
  */
 void rotateActivePiece(Game* game, uint8_t direction);
+
+/**
+ * Checks each row in the game board for any line clears.
+ * If any are found, they are removed and everything above is moved down.
+ * Returns the number of lines cleared.
+ */
+uint8_t processLineClears(Game* game);
 
 #endif
