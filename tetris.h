04871f0ed105df2cf7329805a69b242bf9e4f9a4
Jesse Sheehan <jps111@uclive.ac.nz>
2018-10-08 14:13:49 +1300

    Added some docs for tetris.h

--- tetris.h
+++ tetris.h
@@ -3,6 +3,9 @@
 
 #include <stdint.h>
 
+/**
+ * Represents the Tetrominos
+ */
 #define I 0
 #define T 1
 #define O 2
@@ -11,11 +14,17 @@
 #define L 5
 #define J 6
 
+/**
+ * Represents the different states of rotation that the Tetrominos can be in.
+ */
 #define ROTATE_0 0
 #define ROTATE_90 1
 #define ROTATE_180 2
 #define ROTATE_270 3
 
+/**
+ * Represents the entire state of the game.
+ */
 typedef struct game_s {
     uint8_t board[7][5];
     uint8_t active_piece;
@@ -24,6 +33,9 @@ typedef struct game_s {
     uint16_t score;
 } Game;
 
+/**
+ * Represents a 2D position.
+ */
 typedef struct position_s {
     uint8_t x;
     uint8_t y;
