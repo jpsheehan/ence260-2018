8a2bf73dc095b4546b774f6f76091bdfe45df661
Jesse Sheehan <jps111@uclive.ac.nz>
2018-10-08 14:05:23 +1300

    Added some defines for the tetris pieces and orientations.

--- tetris.h
+++ tetris.h
@@ -3,6 +3,31 @@
 
 #include <stdint.h>
 
+#define I 0
+#define T 1
+#define O 2
+#define S 3
+#define Z 4
+#define L 5
+#define J 6
+
+#define ROTATE_0 0
+#define ROTATE_90 1
+#define ROTATE_180 2
+#define ROTATE_270 3
+
+typedef struct game_s {
+    uint8_t board[7][5];
+    uint8_t active_piece;
+    uint8_t active_orientation;
+    Position active_position;
+} Game;
+
+typedef struct position_s {
+    uint8_t x;
+    uint8_t y;
+} Position;
+
 /**
  * Starts a game of tetris.
  */
