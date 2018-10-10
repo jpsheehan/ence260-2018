5560d130d88f3f6eb94a9bf946eff816624211e8
Jesse Sheehan <jesse@sheehan.nz>
2018-10-10 19:20:18 +1300

    Added rotation and movement to the REPL

--- src/test/test.c
+++ src/test/test.c
@@ -28,7 +28,7 @@ int main(void)
     tetris_init();
 
     Game game = {0};
-    char input[10] = {0};
+    char input[128] = {0};
     uint8_t numRead = 0;
     spawnNextTetromino(&game);
 
@@ -46,16 +46,36 @@ int main(void)
             printf("?, help: displays this message\n");
             printf("q, quit: quits the program\n");
             printf("--------\n");
+            printf("l, left: shifts the active tetromino left\n");
+            printf("r, right: shifts the active tetromino right\n");
             printf("g, gravity: makes the active tetromino fall\n");
+            printf("cw: rotate clockwise\n");
+            printf("ccw: rotate counter-clockwise\n");
         } else if (strcmp(input, "gravity") == 0 || strcmp(input, "g") == 0) {
             applyGravity(&game);
             printf("Applying gravity...\n");
+            display(&game);
+        } else if (strcmp(input, "cw") == 0) {
+
+            printf("Rotating clockwise...\n");
+            display(&game);
+        } else if (strcmp(input, "ccw") == 0) {
+
+            printf("Rotating counter-clockwise...\n");
+            display(&game);
+        } else if (strcmp(input, "l") == 0 || strcmp(input, "left") == 0) {
+
+            printf("Shifting left...\n");
+            display(&game);
+        } else if (strcmp(input, "r") == 0 || strcmp(input, "right") == 0) {
+
+            printf("Shifting right...\n");
             display(&game);
         }
 
         printf("> ");
         numRead = scanf("%s", input);
-        input[9] = '\0';
+        input[127] = '\0';
 
     }
 
