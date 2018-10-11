e2987efcf08c2ad5f777fe8241ec0f4a22d55dd7
Jesse Sheehan <jps111@uclive.ac.nz>
2018-10-11 17:15:16 +1300

    Added the ghost piece back in. It is not yet displayed though. Turned off the blue led on startup

--- src/showScreen.c
+++ src/showScreen.c
@@ -1,6 +1,7 @@
 #include "showScreen.h"
 #include "pio.h"
 #include "../lib/utils/pacer.h"
+#include "tetris.h"
 
 /**
  * initialising columns
@@ -10,19 +11,23 @@ uint8_t j;
 
 void show_screen(uint8_t gameBoard[7][5]) {
     uint8_t cols[5] = {0};
+    static uint8_t numberOfTimesCalled = 0;
     // iterate through columns
     for (i = 0; i < 5; i++) {
         //iterate through rows
         for (j = 0; j < 7; j++) {
             // If pixel is active on gameboard
-            if (gameBoard[j][i]) {
-                cols[i] |= (1 << j);
+            if (gameBoard[j][i] == STACK || gameBoard[j][i] == ACTIVE) {
+                // if (gameBoard[j][i] == GHOST && numberOfTimesCalled % 2 == 0 || gameBoard[j][i] != GHOST) {
+                    cols[i] |= (1 << j);
+                // }
             }
         }
         ledmat_display_column (cols[i], i);
         pacer_wait();
     }
     pio_output_high(LEDMAT_COL5_PIO);
+    numberOfTimesCalled++;
 }
 
 void screen_init() {
