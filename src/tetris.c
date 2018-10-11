461b3ae488cd4f8ebc2b981896883931844781e0
Jesse Sheehan <jps111@uclive.ac.nz>
2018-10-11 17:06:09 +1300

    Removed led turning on and off from the game.

--- src/tetris.c
+++ src/tetris.c
@@ -158,17 +158,9 @@ void check_move(Game* game) {
     } else if (navswitch_push_event_p(NAVSWITCH_WEST)) {
         moveActivePiece(game, LEFT);
     } else if (navswitch_push_event_p(NAVSWITCH_NORTH)) {
-        Orientation oldOrientation = game->active_orientation;
         rotateActivePiece(game, CLOCKWISE);
-        if (oldOrientation == game->active_orientation) {
-            led_set(0, true);            
-        }
     } else if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
         holdPiece(game);
-    }
-
-    if (navswitch_release_event_p(NAVSWITCH_NORTH)) {
-        led_set(0, false);
     }
 }
 #endif
