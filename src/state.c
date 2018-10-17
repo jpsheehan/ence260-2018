bada79d1656688dd3c37b547259b405822c6a16c
Jesse Sheehan <jesse@sheehan.nz>
2018-10-18 08:10:03 +1300

    Documented the state files. Ran astyle on them. Reordered the functions alphabetically.

--- src/state.c
+++ src/state.c
@@ -1,27 +1,37 @@
 /**
- * Tetris Project
- * By Ben Slattery and Jesse Sheehan 2018
+ * state.c
+ *
+ * The state module provides functions and macros for handling the program state.
+ *
+ * ENCE260 Assignment
+ * Written by Ben Slattery and Jesse Sheehan
+ * October 2018
  */
 
 #include "state.h"
 
 /**
- * Stores the state of the game.
+ * Stores the state of the game. We keep this static so that it cannot be directly accessed.
+ * This may be desirable behaviour in the future.
  */
 static uint8_t state;
 
 /**
- * Sets the state of the game to _state.
- */
-void setState(uint8_t _state)
-{
-  state = _state;
-}
-
-/**
- * Returns the state of the game.
+ * Sets the current state of the game.
+ *
+ * @param _state The state you wish to set (STATE_STARTUP, STATE_MENU, STATE_WON, STATE_LOST or STATE_SCORE).
  */
 uint8_t getState(void)
 {
-  return state;
+    return state;
+}
+
+/**
+ * Gets the state of the game.
+ *
+ * @returns The current state of the game.
+ */
+void setState(uint8_t _state)
+{
+    state = _state;
 }
--- src/state.h
+++ src/state.h
@@ -1,6 +1,11 @@
 /**
- * Tetris Project
- * By Ben Slattery and Jesse Sheehan 2018
+ * state.h
+ *
+ * The state module provides functions and macros for handling the program state.
+ *
+ * ENCE260 Assignment
+ * Written by Ben Slattery and Jesse Sheehan
+ * October 2018
  */
 
 #ifndef STATE_H_
@@ -11,20 +16,24 @@
 /**
  * The different states that the game can be in.
  */
-#define STATE_STARTUP 0
-#define STATE_MENU 1
-#define STATE_WON 2
-#define STATE_LOST 3
-#define STATE_SCORE 4
+#define STATE_STARTUP 0 // displaying the startup screen
+#define STATE_MENU 1    // displaying the player selection screen
+#define STATE_WON 2     // displaying the victory screen
+#define STATE_LOST 3    // displaying the loss screen
+#define STATE_SCORE 4   // displaying the score
 
 /**
- * Sets the state of the game to _state.
- */
-void setState(uint8_t _state);
-
-/**
- * Returns the state of the game.
+ * Gets the state of the game.
+ *
+ * @returns The current state of the game.
  */
 uint8_t getState(void);
+
+/**
+ * Sets the current state of the game.
+ *
+ * @param _state The state you wish to set (STATE_STARTUP, STATE_MENU, STATE_WON, STATE_LOST or STATE_SCORE).
+ */
+void setState(uint8_t _state);
 
 #endif
