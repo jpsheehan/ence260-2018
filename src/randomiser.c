2a888be41b6d6b689cb7943d43b70d06f26339de
Jesse Sheehan <jps111@uclive.ac.nz>
2018-10-18 10:40:47 +1300

    Documented the randomiser module. Astyled it.

--- src/randomiser.c
+++ src/randomiser.c
@@ -1,3 +1,12 @@
+/**
+ * randomiser.h
+ *
+ * The randomiser module provides functions and macros for generating random numbers and tetrominos.
+ *
+ * ENCE260 Assignment
+ * Written by Ben Slattery and Jesse Sheehan
+ * October 2018
+ */
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -13,36 +22,55 @@
  * Imagine all 7 different pieces are placed in a bag and drawn at random one-by-one.
  * When you run out of pieces in the bag you put all the pieces back and start again.
  */
+
+/**
+ * The current bag.
+ */
 static uint8_t sevenBag[BAG_SIZE];
+
+/**
+ * The current position in the bag.
+ */
 static uint8_t sevenBagIndex;
 
+/**
+ * Gets whether or not the specific piece exists inside the current bag.
+ * 
+ * @param piece The piece to check for (O, I, T, S, Z, L or J)
+ * @returns true if the piece exists in the bag.
+ */
 bool randomiser_existsInSevenBag(uint8_t piece)
 {
     uint8_t i;
-    for (i = 0; i < BAG_SIZE; i++) {
-        if (sevenBag[i] == piece) {
+    for (i = 0; i < BAG_SIZE; i++)
+    {
+        if (sevenBag[i] == piece)
+        {
             return true;
         }
     }
     return false;
 }
 
-uint16_t randomiser_getRand(void) {
-    return rand() + timer_get();
-}
-
+/**
+ * Destroys the current bag and creates a new one.
+ */
 void randomiser_generateSevenBag(void)
 {
     // reset the contents of the bag
     memset(sevenBag, NONE, BAG_SIZE);
 
     uint8_t i;
-    for (i = 0; i < BAG_SIZE; i++) {
+    for (i = 0; i < BAG_SIZE; i++)
+    {
 
         // pick a piece at random
         uint8_t piece = randomiser_getRand() % NUM_TETROMINOS;
 
-        while (randomiser_existsInSevenBag(piece)) {
+        // if that piece already exists in the bag, draw a new piece until it doesn't exist
+        // there are probably better ways of doing this
+        while (randomiser_existsInSevenBag(piece))
+        {
             piece = randomiser_getRand() % NUM_TETROMINOS;
         }
 
@@ -52,11 +80,25 @@ void randomiser_generateSevenBag(void)
 }
 
 /**
- * Returns a number from 0 - 6 (inclusive) that represents the next tetromino to spawned
+ * Gets the next piece in the bag. If the bag is empty it will generate a new bag behind the scenes.
+ * 
+ * @returns the next piece in the bag.
  */
-uint8_t randomiser_getNextTetromino(void) {
-    if (sevenBagIndex == BAG_SIZE) {
+uint8_t randomiser_getNextTetromino(void)
+{
+    if (sevenBagIndex == BAG_SIZE)
+    {
         randomiser_generateSevenBag();
     }
     return sevenBag[sevenBagIndex++];
+}
+
+/**
+ * Gets a general purpose 16-bit random number.
+ * 
+ * @returns The random number.
+ */
+uint16_t randomiser_getRand(void)
+{
+    return rand() + timer_get();
 }
--- src/randomiser.h
+++ src/randomiser.h
@@ -1,3 +1,13 @@
+/**
+ * randomiser.h
+ *
+ * The randomiser module provides functions and macros for generating random numbers and tetrominos.
+ *
+ * ENCE260 Assignment
+ * Written by Ben Slattery and Jesse Sheehan
+ * October 2018
+ */
+
 #ifndef RANDOMISER_H_
 #define RANDOMISER_H_
 
@@ -5,18 +15,35 @@
 #include <stdbool.h>
 
 /**
- * Related to random piece generation.
+ * The size of the bag defines how many pieces can fit in the bag at any one time.
  */
 #define BAG_SIZE 7
 
-
+/**
+ * Gets whether or not the specific piece exists inside the current bag.
+ * 
+ * @param piece The piece to check for (O, I, T, S, Z, L or J)
+ * @returns true if the piece exists in the bag.
+ */
 bool randomiser_existsInSevenBag(uint8_t piece);
 
-uint16_t randomiser_getRand(void);
-
+/**
+ * Destroys the current bag and creates a new one.
+ */
 void randomiser_generateSevenBag(void);
 
+/**
+ * Gets the next piece in the bag. If the bag is empty it will generate a new bag behind the scenes.
+ * 
+ * @returns the next piece in the bag.
+ */
 uint8_t randomiser_getNextTetromino(void);
 
+/**
+ * Gets a general purpose 16-bit random number.
+ * 
+ * @returns The random number.
+ */
+uint16_t randomiser_getRand(void);
 
 #endif
